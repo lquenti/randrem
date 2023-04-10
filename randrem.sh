b3a97df415772997d2b93c15212817f9bc9c6a12
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-04-10 10:15:33 +0200

    sleep first

--- randrem.sh
+++ randrem.sh
@@ -7,11 +7,11 @@ shift 2
 PARAMETERS=$@
 
 while true; do
-  # send a notification with the parameters
-  notify-send "$PARAMETERS"
-
   # sleep for a random amount of time between MIN_TIME and MAX_TIME seconds
   SLEEP_TIME=$(shuf -i $MINIMUM_TIME-$MAXIMUM_TIME -n 1)
   sleep $SLEEP_TIME
+
+  # send a notification with the parameters
+  notify-send "$PARAMETERS"
 done
 
